9157a259d5e3d17f4804a603276b51f18e246e08
JaydenWen23 <wenjayden23@gmail.com>
2025-11-08 15:39:19 +0700

    te4

--- obj.gd
+++ obj.gd
@@ -19,8 +19,21 @@ func _on_area_2d_mouse_exited():
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("dropable"):
 		is_inside_dropable = true
+		body.modulate = Color(Color.AQUA, 1)
+		body_ref = body_ref
 
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group('dropable'):
+		is_inside_dropable = false
+		body.modulate = Color(Color.BLUE, 1)
+	
+
+func _process(delta):
+	if draggable == true:
 		pass
+		if Input.is_action_pressed('click'):
+			global_position = get_global_mouse_position()
+		elif Input.is_action_just_released("click"):
+			Global.is_dragging = false
+			var tween = get_tree().create_tween()
